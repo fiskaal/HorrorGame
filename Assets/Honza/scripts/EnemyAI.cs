24c6b243f6b7019ee820c0765c3dfac8d178847a
Jan Horký <132677573+GrommyZ@users.noreply.github.com>
2024-04-01 12:01:44 +0200

    copied scene, NavMesh created and tested with basic script

---
+++ Assets/Honza/scripts/EnemyAI.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public Transform player;
+    private NavMeshAgent agent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        agent.destination = player.position;
+    }
+}
