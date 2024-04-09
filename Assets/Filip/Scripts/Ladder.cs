68cedc036a7cc6373101365ae7be0c9f3e288046
fiskaal <filipskala@scholastika.cz>
2024-04-09 18:09:41 +0200

    update
    ladder and openable doors mechanics added

---
+++ Assets/Filip/Scripts/Ladder.cs
@@ -0,0 +1,85 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ladder : MonoBehaviour
+{
+
+    public Transform playerController;
+    bool inside = false;
+    public float speed = 3f;
+    public ExamplePlayerController player;
+    public AudioSource sound;
+
+
+
+
+
+    void Start()
+    {
+        player = GetComponent<ExamplePlayerController>();
+        inside = false;
+
+
+    }
+
+
+
+    void OnTriggerEnter(Collider col)
+    {
+
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderTrue");
+            player.enabled = false;
+            inside = !inside;
+        }
+
+
+    }
+
+    void OnTriggerExit(Collider col)
+    {
+
+        if (col.gameObject.tag == "Ladder")
+        {
+            Debug.Log("TouchingLadderFalse");
+            player.enabled = true;
+            inside = !inside;
+        }
+
+
+    }
+
+    void Update()
+    {
+        if (inside == true && Input.GetKey("w"))
+        {
+            player.transform.position += Vector3.up /
+            speed * Time.deltaTime;
+        }
+
+        if (inside == true && Input.GetKey("s"))
+        {
+            player.transform.position += Vector3.down /
+            speed * Time.deltaTime;
+        }
+
+        if (inside == true && Input.GetKey("w"))
+        {
+            sound.enabled = true;
+            sound.loop = true;
+        }
+        else
+        {
+            sound.enabled = false;
+            sound.loop = false;
+        }
+
+
+
+
+    }
+
+
+}
