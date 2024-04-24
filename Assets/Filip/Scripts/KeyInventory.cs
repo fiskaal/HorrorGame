58a8478be53fa557a6509e53fa3aaa39f5d16e01
fiskaal <filipskala@scholastika.cz>
2024-04-24 17:47:08 +0200

    update
    locked(red) doors added

--- Assets/Filip/Scripts/KeyInventory.cs
+++ Assets/Filip/Scripts/KeyInventory.cs
@@ -2,17 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class KeyInventory : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
+namespace KeySystem
+{ 
+    public class KeyInventory : MonoBehaviour
     {
-        
+        public bool hasRedKey = false;
+        public bool hasOrangeKey = false;
+
     }
 }
