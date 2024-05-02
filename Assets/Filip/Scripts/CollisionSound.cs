50f126f934a49a238badeade7965df0113f3b185
fiskaal <114600610+fiskaal@users.noreply.github.com>
2024-05-02 11:58:42 +0200

    update
    new prefabs, trigger sound mechanic

---
+++ Assets/Filip/Scripts/CollisionSound.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class CollisionSound : MonoBehaviour
+{
+    public AudioClip collisionSound;  // The sound to be played on collision
+    public AudioSource audioSource;   // Reference to the AudioSource component
+    public GameObject gameObject;
+
+
+
+    void Start()
+    {
+        // Initialize the AudioSource component
+        
+
+        // Set the collision sound to the AudioSource
+        audioSource.clip = collisionSound;
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        // Check if the collision involves the desired object
+        if (collision.gameObject.CompareTag("Player"))  // Replace "YourTag" with the tag of the object you want to collide with
+        {
+            // Play the collision sound
+            audioSource.Play();
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            
+            
+            audioSource.Play();
+            gameObject.SetActive(false);
+        }
+
+    }
+}
