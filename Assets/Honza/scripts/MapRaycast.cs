a6e7c4dd6050963d91a19a0601caa7a575f7075b
Jan Horký <132677573+GrommyZ@users.noreply.github.com>
2024-05-21 14:09:12 +0200

    small scene changes + Map VIP

---
+++ Assets/Honza/scripts/MapRaycast.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace KeySystem
+{
+    public class MapRaycast : MonoBehaviour
+    {
+        [SerializeField] private int rayLength = 5;
+        [SerializeField] private LayerMask layerMaskInteract;
+        [SerializeField] private string excludeLayerName = null;
+
+        private KeyItemController raycastedObject;
+
+        [SerializeField] private KeyCode openDoorKey = KeyCode.E;
+
+        [SerializeField] private Image crosshair;
+        [SerializeField] private GameObject imageE;
+        private bool isCrosshairActive;
+        private bool doOnce;
+
+        private string interactableTag = "InteractiveObject";
+
+        private void Update()
+        {
+            RaycastHit hit;
+            Vector3 fwd = transform.TransformDirection(Vector3.forward);
+
+            int mask = 1 << LayerMask.NameToLayer(excludeLayerName) | layerMaskInteract.value;
+
+            if (Physics.Raycast(transform.position, fwd, out hit, rayLength, mask))
+            {
+                if (hit.collider.CompareTag(interactableTag))
+                {
+                    if (!doOnce)
+                    {
+                        raycastedObject = hit.collider.gameObject.GetComponent<KeyItemController>();
+                        CrosshairChange(true);
+                    }
+                    isCrosshairActive = true;
+                    doOnce = true;
+
+                    if (Input.GetKeyDown(openDoorKey))
+                    {
+                        raycastedObject.ObjectInteraction();
+                    }
+                }
+            }
+            else
+            {
+                if (isCrosshairActive)
+                {
+                    CrosshairChange(false);
+                    doOnce = false;
+                }
+            }
+        }
+
+        void CrosshairChange(bool on)
+        {
+            if (on)
+            {
+                imageE.SetActive(true);
+            }
+            else
+            {
+                imageE.SetActive(false);
+            }
+        }
+    }
+}
