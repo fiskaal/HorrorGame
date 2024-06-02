65228ac478167355c02b87dbc1c6d65bdce837e0
fiskaal <filipskala@scholastika.cz>
2024-06-02 21:41:14 +0200

    update
    flashlight offset added

---
+++ Assets/Filip/Scripts/FlashLightOffset.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FlashLightOffset : MonoBehaviour
+{
+    [SerializeField]private Vector3 vectOffset;
+    [SerializeField]private GameObject goFollow;
+    [SerializeField] private float speed = 3.0f;
+
+    
+    private void Start()
+    {
+        goFollow = Camera.main.gameObject;
+        vectOffset = transform.position - goFollow.transform.position;
+    }
+
+    private void Update()
+    {
+        transform.position = goFollow.transform.position + vectOffset;
+        transform.rotation = Quaternion.Slerp(transform.rotation, goFollow.transform.rotation, speed * Time.deltaTime);
+    }
+}
