0ad5161d9ad75f5836684bd3b12cc14148b90885
fiskaal <114600610+fiskaal@users.noreply.github.com>
2024-06-15 21:54:55 +0200

    update
    new prefabs

--- Assets/Filip/Scripts/QuestSystem.cs
+++ Assets/Filip/Scripts/QuestSystem.cs
@@ -7,37 +7,14 @@ using UnityEngine.Events;
 
 public class QuestSystem : MonoBehaviour
 {
-    public AudioClip newQuestSound;  // The sound to be played on collision
-    public AudioSource audioSource;   // Reference to the AudioSource component
     public GameObject hideCurrentQuest;
     public GameObject showNextQuest;
-    //public GameObject currentQuestText;
-    //public GameObject nextQuestText;
-
+    
     [SerializeField] private TMP_Text noteTextAreaUI;
     [SerializeField] [TextArea] private string questText;
     [SerializeField] private UnityEvent newQuestSoundEvent;
 
-    private void Awake()
-    {
-        //noteTextAreaUI.text = null;
-    }
-
-    void Start()
-    {
-        // Initialize the AudioSource component
-
-
-        // Set the collision sound to the AudioSource
-        //audioSource.clip = newQuestSound;
-        //noteTextAreaUI.text = "";
-    }
-
     
-
-
-
-
 
     private void OnTriggerEnter(Collider other)
     {
@@ -46,14 +23,9 @@ public class QuestSystem : MonoBehaviour
 
             newQuestSoundEvent.Invoke();
             noteTextAreaUI.text = questText;
-            //audioSource.Play();
             hideCurrentQuest.SetActive(false);
-            //currentQuestText.SetActive(false);
             showNextQuest.SetActive(true);
-            //nextQuestText.SetActive(true);
             
-            
-
 
         }
 
